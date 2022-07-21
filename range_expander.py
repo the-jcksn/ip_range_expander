fde7507e17c5a9f7de0e4364ebcef42d7485153d
the-jcksn <101604447+the-jcksn@users.noreply.github.com>
2022-07-21 04:31:01 -0400

    Update range_expander.py

--- range_expander.py
+++ range_expander.py
@@ -1,5 +1,5 @@
-ip_start = input('Please enter the first 3 octets (eg 192.168.0): ')
-last_octet = input('Please enter the range of the last octet seperated by a comma (eg 1,254): ')
+ip_start = input('Enter the first 3 octets (eg 192.168.0): ')
+last_octet = input('Enter the range of the last octet seperated by a comma (eg 1,254): ')
 
 range_split = last_octet.split(',')
 first = int(range_split[0])
